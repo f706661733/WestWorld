97b5e85a32cd51ba47ee8b7f0a5f7a56c80efec6
sevan <fanmaozhi@hotmail.com>
2018-05-27 21:16:43 +0800

    TEst

---
+++ src/main/java/org/qiqi/server/ServerClient.java
@@ -0,0 +1,11 @@
+package org.qiqi.server;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Service;
+
+@EnableAutoConfiguration
+@Service
+public class ServerClient {
+
+
+}
